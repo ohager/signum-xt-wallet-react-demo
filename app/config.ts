cd3b42bc010ca2b69e825250b57ace23f107c977
ohager <oliver@devbutze.com>
2022-04-28 08:41:24 -0300

    fix: remove trailing slash for explorer url

--- app/config.ts
+++ app/config.ts
@@ -1,10 +1,12 @@
 const toBoolean = (v: string): boolean => v.toLowerCase() === "true";
 
+const withoutTrailingSlash = (url: string) => url.endsWith('/') ? url.substring(0, url.length -1) : url
+
 export const Config = {
   DAppName: process.env.NEXT_PUBLIC_DAPP_NAME || "MyDApp",
   Ledger: {
     IsTestnet: toBoolean(process.env.NEXT_PUBLIC_SIGNUM_IS_TESTNET || "false"),
     Network: process.env.NEXT_PUBLIC_SIGNUM_NETWORK || "Signum-TESTNET",
-    Explorer: process.env.NEXT_PUBLIC_SIGNUM_EXPLORER || "https://t-chain.signum.network",
+    Explorer: withoutTrailingSlash(process.env.NEXT_PUBLIC_SIGNUM_EXPLORER || "https://t-chain.signum.network/"),
   },
 };
